a28087dbcef6f92d88b774af41c8f2f1c1913c53
Jinuk <wfo3977@gmail.com>
2021-11-27 11:55:11 +0900

    init project

---
+++ src/main/java/kr/bos/BosApplication.java
@@ -0,0 +1,12 @@
+package kr.bos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BosApplication.class);
+    }
+}
